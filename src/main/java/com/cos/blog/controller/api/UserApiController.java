7d7df208a94df6ea08a81da4a5c01ecd1b3c8bab
kms20a <kms21@naver.com>
2020-12-06 01:47:48 +0900

    전통적인 spring 로그인 방식

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +19,30 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	//스프링 IoC 컨테이너에서 가지고 있는 Bean을 선언
+	@Autowired
+	private HttpSession session;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController : save 호출됨");
 		//실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
 		user.setRole(RoleType.USER);
-		int result = userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK,  result); // 자바오브젝트를 JSON으로 변환해서 리턴
+		userService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),  1); // 자바오브젝트를 JSON으로 변환해서 리턴
+	}
+	
+	// 다음시간에 스프링 시큐리티 이용해서 로그인!!
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user) {
+		System.out.println("UserApiController : login호출됨");
+		User principal = userService.로그인(user);	//principal(접근주체)
+		
+		if(principal != null) {
+			//세션 생성
+			session.setAttribute("principal", principal);
+		}
+		
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -5,6 +5,9 @@ let index = {
 		$("#btn-save").on("click", ()=>{
 			this.save();
 		});
+		$("#btn-login").on("click", ()=>{
+			this.login();
+		});
 	},
 	/*
 	function을 선언하고 내부에서 this를 사용하는 경우는
@@ -44,6 +47,36 @@ let index = {
 			dataType: "json" // 요청을 서버로해서 응답이 왔을 떄 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트로 변경해줌 
 		}).done(function(resp){
 			alert("회원가입이 완료되었습니다.");
+			location.href="/blog";
+			//alert(resp);
+			console(resp);
+		}).fail(function(error){
+			alert(JSON.stringify(error));
+		}); 
+	},
+	login: function(){
+		//alert('user의 save함수 호출됨');
+		// username, password, email 의 값을 찾아서
+		// object 객체로 만들어 준다.
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val()
+		}
+		
+		//브라우저 콘솔로 데이터 확인
+		//console.log(data);
+		
+		//ajax 호출시 default가 비동기 호출
+		//ajax 통신을 이용해서 2개의 데이터를 json으로 변경하여 login 요청
+		$.ajax({
+			//회원가입 수행 요청
+			type: "POST",
+			url: "/blog/api/user/login",
+			data: JSON.stringify(data), //http body데이터
+			contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
+			dataType: "json" // 요청을 서버로해서 응답이 왔을 떄 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트로 변경해줌 
+		}).done(function(resp){
+			alert("로그인이 완료되었습니다.");
 			location.href="/blog";
 			//alert(resp);
 			console(resp);
