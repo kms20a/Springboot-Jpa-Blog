4902087dd1a315ef2ed5d61953c96553627476f0
kms20a <kms21@naver.com>
2020-12-06 01:56:46 +0900

    blog contextpath 삭제

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -41,13 +41,13 @@ let index = {
 		$.ajax({
 			//회원가입 수행 요청
 			type: "POST",
-			url: "/blog/api/user",
+			url: "/api/user",
 			data: JSON.stringify(data), //http body데이터
 			contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
 			dataType: "json" // 요청을 서버로해서 응답이 왔을 떄 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트로 변경해줌 
 		}).done(function(resp){
 			alert("회원가입이 완료되었습니다.");
-			location.href="/blog";
+			location.href="/";
 			//alert(resp);
 			console(resp);
 		}).fail(function(error){
@@ -71,13 +71,13 @@ let index = {
 		$.ajax({
 			//회원가입 수행 요청
 			type: "POST",
-			url: "/blog/api/user/login",
+			url: "/api/user/login",
 			data: JSON.stringify(data), //http body데이터
 			contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
 			dataType: "json" // 요청을 서버로해서 응답이 왔을 떄 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 오브젝트로 변경해줌 
 		}).done(function(resp){
 			alert("로그인이 완료되었습니다.");
-			location.href="/blog";
+			location.href="/";
 			//alert(resp);
 			console(resp);
 		}).fail(function(error){
