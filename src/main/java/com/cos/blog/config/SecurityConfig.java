d2f0a391db60924d00c6fc88f45bce364aaa4962
kms20a <kms21@naver.com>
2020-12-06 22:32:16 +0900

    password hash

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //빈등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
 @Configuration	// 빈등록(IoC관리)
 @EnableWebSecurity	// 시큐리티 필터로 등록이 된다.
 @EnableGlobalMethodSecurity(prePostEnabled = true) //특정 주소로 접근하면 권한 및 인증을 미리 체크하겠다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean // IoC가 되요!!
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
